75a4fece737c958cc98e2db9a47101b8feeac13f
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-24 17:06:09 -0400

    Finish CRUD operations with typescript node

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createTodo } from "../controllers/todos";
+import {
+  createTodo,
+  deleteToDo,
+  getTodos,
+  updateToDo,
+} from "../controllers/todos";
 
 const router = Router();
 
 router.post("/", createTodo);
-router.get("/");
-router.patch("/:id");
-router.delete("/:id");
+router.get("/", getTodos);
+router.patch("/:id", updateToDo);
+router.delete("/:id", deleteToDo);
 
 export default router;
